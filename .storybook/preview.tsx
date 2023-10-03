853f9ec6260945876774e2aa171ba3db8b468e09
ben-mobi <ben@takemobi.com>
2023-10-03 14:44:19 -0400

    Add font loader to storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,13 +1,60 @@
 // Import styles of packages that you've installed.
 // All packages except `@mantine/hooks` require styles imports
 import "@mantine/core/styles.css";
-import React from "react";
-
+import React, { PropsWithChildren } from "react";
 import { theme } from "../src/styles/theme";
 import { MantineProvider } from "@mantine/core";
+import localFont from "next/font/local";
+
+// Have to load localFont for storybook using different paths than the rest of the app
+const dmSans = localFont({
+  src: [
+    {
+      path: "../fonts/DM_Sans/DMSans-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/DM_Sans/DMSans-Italic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../fonts/DM_Sans/DMSans-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../fonts/DM_Sans/DMSans-MediumItalic.ttf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../fonts/DM_Sans/DMSans-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../fonts/DM_Sans/DMSans-BoldItalic.ttf",
+      weight: "700",
+      style: "italic",
+    },
+  ],
+  variable: "--DM-Sans",
+});
+
+const FontVariableSetter = (props: PropsWithChildren) => {
+  React.useEffect(() => {
+    document.documentElement.classList.add(dmSans.variable);
+  }, []);
+
+  return <>{props.children}</>;
+};
 
 export const decorators = [
   (renderStory) => (
-    <MantineProvider theme={theme}>{renderStory()}</MantineProvider>
+    <FontVariableSetter>
+      <MantineProvider theme={theme}>{renderStory()}</MantineProvider>
+    </FontVariableSetter>
   ),
 ];
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,48 +2,12 @@ import "@mantine/core/styles.css";
 import React from "react";
 import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 import { theme } from "@src/styles/theme";
-import localFont from "next/font/local";
+import { dmSans } from "@src/styles/font";
 
 export const metadata = {
   title: "Mobi Next.js Template",
   description: "I am using Mantine with Next.js!",
 };
-
-const dmSans = localFont({
-  src: [
-    {
-      path: "../../public/fonts/DM_Sans/DMSans-Regular.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/DM_Sans/DMSans-Italic.ttf",
-      weight: "400",
-      style: "italic",
-    },
-    {
-      path: "../../public/fonts/DM_Sans/DMSans-Medium.ttf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/DM_Sans/DMSans-MediumItalic.ttf",
-      weight: "500",
-      style: "italic",
-    },
-    {
-      path: "../../public/fonts/DM_Sans/DMSans-Bold.ttf",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/DM_Sans/DMSans-BoldItalic.ttf",
-      weight: "700",
-      style: "italic",
-    },
-  ],
-  variable: "--DM-Sans",
-});
 
 export default function RootLayout({ children }: { children: any }) {
   return (
