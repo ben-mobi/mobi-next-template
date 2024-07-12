0a287794635bfe2008a0e5c47f7c50dfd59eb417
ben-mobi <ben@takemobi.com>
2024-07-12 11:08:04 -0400

    Update nextjs and storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,80 +2,13 @@
 // All packages except `@mantine/hooks` require styles imports
 import "@mantine/core/styles.css";
 import "../src/styles/css/global.css";
-import React, { PropsWithChildren } from "react";
+import "./storybook-fonts.css";
+import React from "react";
 import { theme } from "../src/styles/theme";
 import { MantineProvider } from "@mantine/core";
-import localFont from "next/font/local";
-
-// Have to load localFont for storybook using different paths than the rest of the app
-const radial = localFont({
-  src: [
-    {
-      path: "../fonts/Radial-Family/Radial-Regular.otf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../fonts/Radial-Family/Radial-Italic.otf",
-      weight: "400",
-      style: "italic",
-    },
-    {
-      path: "../fonts/Radial-Family/Radial-SemiBold.otf",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../fonts/Radial-Family/Radial-SemiBoldItalic.otf",
-      weight: "600",
-      style: "italic",
-    },
-    {
-      path: "../fonts/Radial-Family/Radial-Bold.otf",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../fonts/Radial-Family/Radial-BoldItalic.otf",
-      weight: "700",
-      style: "italic",
-    },
-    {
-      path: "../fonts/Radial-Family/Radial-Black.otf",
-      weight: "800",
-      style: "normal",
-    },
-    {
-      path: "../fonts/Radial-Family/Radial-BlackItalic.otf",
-      weight: "800",
-      style: "italic",
-    },
-    {
-      path: "../fonts/Radial-Family/Radial-Heavy.otf",
-      weight: "900",
-      style: "normal",
-    },
-    {
-      path: "../fonts/Radial-Family/Radial-HeavyItalic.otf",
-      weight: "900",
-      style: "italic",
-    },
-  ],
-  variable: "--Radial",
-});
-
-const FontVariableSetter = (props: PropsWithChildren) => {
-  React.useEffect(() => {
-    document.documentElement.classList.add(radial.variable);
-  }, []);
-
-  return <>{props.children}</>;
-};
 
 export const decorators = [
   (renderStory) => (
-    <FontVariableSetter>
-      <MantineProvider theme={theme}>{renderStory()}</MantineProvider>
-    </FontVariableSetter>
+    <MantineProvider theme={theme}>{renderStory()}</MantineProvider>
   ),
 ];
